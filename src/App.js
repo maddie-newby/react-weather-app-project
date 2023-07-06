b0969ec6da47b94368648cc4e134e4fc71c27758
maddie-newby <madison1999@hotmail.co.uk>
2023-07-06 12:40:23 +0100

    Remove error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
 
@@ -8,6 +7,6 @@ export default function App() {
         <h1>Enter a city</h1>
         <Weather city="New York"/>
 <footer>This project was coded by Madison Newby and is hosted on <a href='https://github.com/maddie-newby/react-weather-app-project'>GitHub</a></footer>
-    </div>>
+    </div>
   );
 };
