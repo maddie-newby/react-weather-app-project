c99a4c0a55259191153fbe0c9e8fc1e86ce145b2
maddie-newby <madison1999@hotmail.co.uk>
2023-07-06 12:35:05 +0100

    add sandbox project

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Enter a city</h1>
         <Weather city="New York"/>
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(city) {
-  function handleResponse(response) {
-    alert(`The weather in Bristol is ${response.data.main.temp} degrees`)
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "f5029b784306910c19746e40c14d6cd3";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        setMessage(
+          <div>
+            <h2>{response.data.name}</h2>
+          <ul>
+            <li>Temperature: {Math.round(response.data.main.temp)} °C</li>
+            <li>Description: {response.data.weather[0].description}</li>
+            <li>Humidity: {response.data.main.humidity} %</li>
+            <li>
+              Wind:{" "}
+              {Math.round(
+                (response.data.wind.speed * 3.6 + Number.EPSILON) * 100
+              ) / 100}{" "}
+              km/h
+            </li>
+            <li>
+              <img
+                src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+                alt={response.data.weather[0].description}
+              />
+            </li>
+          </ul>
+          </div>
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-    let apiKey="3c949ba49d38be2487ee278e0d2d4059";
-    let apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; 
-    axios.get(apiUrl).then(handleResponse);
-    return <h2>Weather</h2>;
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="WeatherSearch">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={changeCity} />
+        <input type="submit" value="Search" />
+      </form>
+      {message && <h2>{message}</h2>}
+    </div>
+  );
 }
